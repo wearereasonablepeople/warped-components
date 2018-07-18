b46971bfa12134ab4805ff5bb8d7bf49604350d2
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2018-07-18 13:33:17 +0200

    Suppress unresolved dependency warning on build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,10 @@ import pkg from './package.json';
 
 export default {
   input: 'index.mjs',
-  external: Object.keys (pkg.peerDependencies),
+  external: (
+    Object.keys (pkg.peerDependencies)
+    .concat (Object.keys (pkg.dependencies))
+  ),
   output: {
     format: 'umd',
     name: 'warpedComponents',
